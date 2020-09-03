66269a854e4e0ef9e53e86d20e62fb3f3e5fb7a6
Ehsan Shahbazi <meshahbazi72@gmail.com>
2020-09-03 23:21:07 +0430

    new_pics

--- goldproj/go_on.py
+++ goldproj/go_on.py
@@ -1,6 +1,19 @@
 import requests
+from requests.exceptions import ConnectionError, ConnectTimeout
 import time
 
+
+def do_the_job():
+    try:
+        requests.get('http://185.110.189.178:8000/do_your_job_man')
+        time.sleep(60)
+        return True
+    except (ConnectTimeout, ConnectionError):
+        do_the_job()
+    finally:
+        return True
+
+
 while True:
-    time.sleep(60)
-    requests.get('http://185.110.189.178:8000/do_your_job_man')
+    do_the_job()
+    time.sleep(1)
